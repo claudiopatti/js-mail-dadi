df9ada91bf41e16b3c1c93403b7da6017c478394
claudiopatti <claudiopatti1997@gmail.com>
2024-07-17 01:32:06 +0200

    gioco mail riportato in pagina manca il layout da sistemare

--- MAIL/js/scripts.js
+++ MAIL/js/scripts.js
@@ -1,25 +1,65 @@
-console.log('ciao console')
+console.log('ciao console');
 
-const email = prompt("Qual'è la vostra email?")
-console.log('Email', email , typeof email)
+// const email = prompt("Qual'è la vostra email?")
+// console.log('Email', email , typeof email);
 
-const enterEmail = [
-    "serenovariabile@gmail.com",
-    "ernestotrottola@gmail.com",
-    "carlossantos@gmail.com",
-    "pierolino@gmail.com",
-    "paololoto@gmail.com"
-]
 
-for (let i = 0; i < enterEmail.length; i++ ){
-    // console.log('enterEmail[i]' , enterEmail[i] , typeof enterEmail[i])
+// const enterEmail = [
+//     "serenovariabile@gmail.com",
+//     "ernestotrottola@gmail.com",
+//     "carlossantos@gmail.com",
+//     "pierolino@gmail.com",
+//     "paololoto@gmail.com"
+// ]
+
+// for (let i = 0; i < enterEmail.length; i++ ){
+//     // console.log('enterEmail[i]' , enterEmail[i] , typeof enterEmail[i])
    
-    if (email == enterEmail[i]) {
-        console.log(enterEmail[i])
-        alert('Sei uno dei nostri' + " " + email)
+//     if (email == enterEmail[i]) {
+//         console.log(enterEmail[i])
+//         alert('Sei uno dei nostri' + " " + email)
+//     }
+//     else {
+//         console.log('email non presente' )
+//         alert('Non ti è permesso entrare')
+//     }
+// }
+
+
+
+const infoForm = document.querySelector('form');
+infoForm.addEventListener('submit' ,
+    function (event) {
+        event.preventDefault();
+
+        const enterEmail = [
+            "serenovariabile@gmail.com",
+            "ernestotrottola@gmail.com",
+            "carlossantos@gmail.com",
+            "pierolino@gmail.com",
+            "paololoto@gmail.com"
+        ]
+
+        const emailInput = document.getElementById('emailInput');
+        console.log('emailInput.value', emailInput.value);
+        
+        
+        for (let i = 0; i < enterEmail.length; i++ ){
+            // console.log('enterEmail[i]' , enterEmail[i] , typeof enterEmail[i])
+        
+            if (emailInput.value == enterEmail[i]) {
+                console.log(enterEmail[i])
+                document.getElementById('resultEmail').innerHTML = 'E\' corretta' ;
+                return i ;
+
+                // alert('Sei uno dei nostri' + " " + emailInput.value)
+            }
+            else {
+                console.log('email non presente' )
+                document.getElementById('resultEmail').innerHTML = 'E\' sbagliata' ;
+                // alert('Non ti è permesso entrare')
+            }
+        }
     }
-    else {
-        console.log('email non presente' )
-        alert('Non ti è permesso entrare')
-    }
-}
+
+)
