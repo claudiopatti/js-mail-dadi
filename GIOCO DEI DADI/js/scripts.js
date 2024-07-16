2f8dad952da01008806710928daacf419455b2a8
claudiopatti <claudiopatti1997@gmail.com>
2024-07-17 00:11:13 +0200

    aggiunta risposta visibile in pagina per gioco dei dadi

--- GIOCO DEI DADI/js/scripts.js
+++ GIOCO DEI DADI/js/scripts.js
@@ -1,1 +1,26 @@
 console.log('ciao console')
+
+
+const numberPerson = Math.floor(Math.random() * 6) + 1;
+const numberRandomPerson = document.querySelector('.person');
+numberRandomPerson.innerHTML = numberPerson
+console.log('numberPerson', numberPerson, typeof numberPerson);
+
+const numberComputer = Math.floor(Math.random() * 6) + 1;
+const numberRandomComputer = document.querySelector('.computer');
+numberRandomComputer.innerHTML = numberComputer
+console.log('numberComputer', numberComputer, typeof numberComputer);
+
+if (numberPerson > numberComputer ) {
+    document.getElementById('resultMatch').innerHTML = 'Il vincitore è la persona'
+    console.log('il vincitore è la persona');
+    
+}else if (numberComputer > numberPerson) {
+    document.getElementById('resultMatch').innerHTML = 'Il vincitore è il computer'
+    console.log('il vincitore è il computer');
+    
+}
+else {
+    document.getElementById('resultMatch').innerHTML = 'E\' pari'
+    console.log('è pari');
+}
--- README.md
+++ README.md
@@ -10,3 +10,14 @@ stampa un messaggio appropriato sull'esito del controllo.
 3. stampare il risultato dell'esito
     a. se l'email data è nella lista il risultato sarà un messaggio con la possibilità di accesso
     b. altrimenti se l'email non fa parte della lista 
+
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+1. genera 2 numeri random per il computer e per il giocatore che vadano da 1 a 6
+2. stabillire il vincitore in base al numero maggiore
